4ebfdd0c32b2eebfaef4f7d606f008e5d77a9e1d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-11 22:55:39 -0600

    udpate

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      <p>This is your dashboard page.</p>
+      <p>You can add more content here, such as charts, tables, or other components.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DashboardPage' // Update the component name to "DashboardPage"
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your custom styles here */
+  </style>
+  
