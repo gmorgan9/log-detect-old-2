db5dfa34e01c086b974a141c4e6dfc083eac5db0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-11 19:37:43 -0600

    update

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue'
+import LoginForm from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    Login
+    LoginForm
   }
 }
 </script>
