22279cb2fafff04285c4396968aab5a45358d2b4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-12 10:45:39 -0600

    update

--- src/router.js
+++ src/router.js
@@ -1,23 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router';
+// import { createRouter, createWebHistory } from 'vue-router';
+// import LoginPage from './components/LoginPage.vue';
+// import DashboardPage from './components/Dashboard.vue';
+
+// const routes = [
+//   {
+//     path: '/login',
+//     name: 'Login',
+//     component: LoginPage
+//   },
+//   {
+//     path: '/dashboard',
+//     name: 'Dashboard',
+//     component: DashboardPage
+//   }
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes
+// });
+
+// export default router;
+
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import LoginPage from './components/LoginPage.vue';
 import DashboardPage from './components/Dashboard.vue';
 
-const routes = [
-  {
-    path: '/login',
-    name: 'Login',
-    component: LoginPage
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardPage
-  }
-];
+Vue.use(VueRouter);
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashboardPage
+    }
+  ]
 });
 
 export default router;
